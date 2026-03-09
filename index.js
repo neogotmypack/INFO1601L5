458391270e8a774d88e90ef32ab9c0c3dc97b77e
neogotmypack <rialations@gmail.com>
2026-03-09 01:04:32 -0400

    INFO1601L5

--- index.js
+++ index.js
@@ -1,5 +1,92 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// 10 elements
 
-for(let i=0; i< arr.length; i++){
-  console.log(arr[i])
+let bob = {
+  fname: "bob",
+  lname: "smith",
+  age: 18,
+  height: 6,
+  transcript:[
+    {
+      course: 'INFO 1603',
+      grades: [ 89, 34, 67 ]
+    },
+    {
+      course: 'INFO 1601',
+      grades: [ 89, 34, 67 ]
+    }
+  ]
+};
+
+let sally = {
+  fname: "sally",
+  lname: "smith",
+  age: 18,
+  height: 6,
+  transcript:[
+    {
+      course: 'INFO 1601',
+      grades: [ 100, 89, 79 ]
+    }
+  ]
+};
+
+let paul = {
+  fname: "paul",
+  lname: "smith",
+  age: 18,
+  height: 6,
+  transcript:[
+    {
+      course: 'INFO 1600',
+      grades: [ 89, 34, 67 ]
+    }
+  ]
+};
+
+
+const students = [bob, sally, paul];
+
+function getAverageGrade(student, course) {
+  for (let i = 0; i < student.transcript.length; i++) {
+    if (student.transcript[i].course === course) {
+      let grades = student.transcript[i].grades;
+      let sum = 0;
+      for (let j = 0; j < grades.length; j++) {
+        sum += grades[j];
+      }
+      return sum / grades.length;
+    }
+  }
+  return -1;
+}
+
+function getAssignmentMark(student, course, num) {
+  for (let i = 0; i < student.transcript.length; i++) {
+    if (student.transcript[i].course === course) {
+      if (num >= 0 && num < student.transcript[i].grades.length) {
+        return student.transcript[i].grades[num];
+      }
+    }
+  }
+  return -1;
+}
+
+function averageAssessment(students, courseName, assignment) {
+  let sum = 0;
+  let count = 0;
+  
+  for (let i = 0; i < students.length; i++) {
+    for (let j = 0; j < students[i].transcript.length; j++) {
+      if (students[i].transcript[j].course === courseName) {
+        if (assignment >= 0 && assignment < students[i].transcript[j].grades.length) {
+          sum += students[i].transcript[j].grades[assignment];
+          count++;
+        }
+      }
+    }
+  }
+  
+  if (count === 0) {
+    return -1;
+  }
+  return sum / count;
 }
